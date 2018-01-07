4a07e284e3e434c7d56042e8023a03df6aee4a9c
Aleksey <foregestore@gmail.com>
2018-01-08 02:47:25 +0400

    Add normalize.css

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMerge = require('webpack-merge');
 const scss = require('./webpack_conf/sass');
+const css = require('./webpack_conf/css');
 
 const PATHS = {
     development: path.join(__dirname, 'Development/Component/App'),
@@ -26,12 +27,14 @@ module.exports = function (env) {
     if(env === 'production') {
         return WebpackMerge([
             common,
+            css(),
             scss()
         ])
     }
     if(env === 'development') {
         return WebpackMerge([
             common,
+            css(),
             scss()
         ])
     }
