d6208c2ee17fda0bcda4efbcbd5046d8c4c8b565
Aleksey <foregestore@gmail.com>
2018-01-08 03:04:48 +0400

    Add minification

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMerge = require('webpack-merge');
 const scss = require('./webpack_conf/sass');
 const css = require('./webpack_conf/css');
+const uglifyJs = require('./webpack_conf/js.uglify');
 
 const PATHS = {
     development: path.join(__dirname, 'Development/Component/App'),
@@ -19,7 +21,10 @@ const common = {
         new HtmlWebpackPlugin({
             title: 'React App',
             filename: PATHS.production + '/html/index.html'
-        })
+        }),
+        // new webpack.optimize.CommonsChunkPlugin({
+        //     name: 'common'
+        // })
     ]
 };
 
@@ -28,7 +33,8 @@ module.exports = function (env) {
         return WebpackMerge([
             common,
             css(),
-            scss()
+            scss(),
+            uglifyJs()
         ])
     }
     if(env === 'development') {
